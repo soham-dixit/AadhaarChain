62063715eb2d85fe53a5614b15686313a45c0601
soham-1902 <sohampande1902@gmail.com>
2023-12-08 12:27:22 -0800

    Added Login UI

--- citizen_app/lib/screens/login.dart
+++ citizen_app/lib/screens/login.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_app/widgets/neumorphic_textfield.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,16 +18,49 @@ class _LoginPageState extends State<LoginPage> {
       body: NeumorphicApp(
         darkTheme: NeumorphicThemeData(
           baseColor: Color(0xFF3E3E3E),
-          lightSource: LightSource.topLeft,
-          depth: 6,
+          lightSource: LightSource.top,
+          depth: 2,
+          textTheme: GoogleFonts.nunitoSansTextTheme(),
         ),
         home: NeumorphicBackground(
-          child: Center(
-            child: NeumorphicButton(
-              onPressed: () {},
-              child: const Text('Click Me'),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(),
+                Column(
+                  children: [
+                    NeumorphicTextField(hintText: 'Email', iconData: Icons.email),
+                    SizedBox(height: 20,),
+                    NeumorphicTextField(hintText: 'Password', iconData: Icons.password),
+                  ],
+                ),
+                NeumorphicButton(
+                  onPressed: () {
+                    Get.to('/');
+                  },
+                  child: Text(
+                    '            Login            ',
+                    style: TextStyle(
+                        color: Colors.white
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+          /*child: Center(
+            child: NeumorphicButton(
+              onPressed: () {},
+              child: Text(
+                  'Click Me',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+              ),
+            ),
+          ),*/
         ),
       ),
     );
