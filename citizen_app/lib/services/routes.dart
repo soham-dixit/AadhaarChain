07043e07628e0bf276fc1d2825bb66a109076c39
sohamdixit <sohamddixit@gmail.com>
2023-12-09 01:58:24 +0530

    added maps

--- citizen_app/lib/services/routes.dart
+++ citizen_app/lib/services/routes.dart
@@ -1,11 +1,16 @@
 import 'package:get/get.dart';
+import 'package:mobile_app/screens/home.dart';
 import 'package:mobile_app/screens/login.dart';
 
 appRoutes() => [
       GetPage(
         name: '/login',
-        page: () => const LoginPage(),
+        page: () => LoginPage(),
       ),
+      GetPage(
+        name: '/home',
+        page: () => HomePage(),
+      )
     ];
 
 class MyMiddelware extends GetMiddleware {
