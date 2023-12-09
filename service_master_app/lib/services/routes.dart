06d7ceee78a0acb1ac17ba6b8e680b05c891ac69
sohamdixit <sohamddixit@gmail.com>
2023-12-09 11:46:47 +0530

    service master register

--- service_master_app/lib/services/routes.dart
+++ service_master_app/lib/services/routes.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 import 'package:service_master_app/screens/login.dart';
+import 'package:service_master_app/screens/register.dart';
 
 appRoutes() => [
       GetPage(
         name: '/login',
         page: () => LoginPage(),
+      ),
+      GetPage(
+        name: '/register',
+        page: () => RegisterPage(),
       ),
     ];
 
